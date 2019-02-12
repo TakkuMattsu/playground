fe7c6caf87375353862e6f1431bbf930ab8cd313
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2019-02-12 15:00:39 +0900

    Array<Observable<T>> を Observable<T> に変換する

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+
+let listObservable: Array<Observable<Int>> = [Observable.just(1), Observable.just(2)]
+let observableFromList: Observable<Int> = Observable.concat(listObservable)
+observableFromList.subscribe(onNext: {
+    print($0)
+})
+
+let kadai: Observable<Array<Int>> = Observable.create { observer in
+    observer.onNext([1,2,3])
+    observer.onCompleted()
+    return Disposables.create()
+}
+kadai.flatMap { (list: Array<Int>) -> Observable<Int> in
+    let obs: Array<Observable<Int>> = list.map({ (v: Int) -> Observable<Int> in
+        Observable.just(v)
+    })
+    return Observable.concat(obs)
+    }.subscribe(onNext: {
+        print("hello")
+        print($0)
+    })
